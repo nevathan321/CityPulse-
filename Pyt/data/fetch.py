25ab5a268692e559cb7d611fc2900bb5a175ce3c
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2025-06-21 06:30:44 -0400

    Completed visualization

--- Pyt/data/fetch.py
+++ Pyt/data/fetch.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
-
-df = pd.read_csv(r"Pyt\SR2025.csv", on_bad_lines='skip', encoding='latin1')
-
+### DO NOT EXECUTE
+df = pd.read_csv(r"Pyt\data\SR2025.csv", on_bad_lines='skip', encoding='latin1')
 df_cleaned = df[df['Status'].str.strip().str.lower() != 'completed']
-
-df_cleaned.to_csv(r"Pyt\SR2025_cleaned.csv", index=False)
+df_cleaned.to_csv(r"Pyt\data\SR2025_cleaned.csv", index=False)
 
 
 
